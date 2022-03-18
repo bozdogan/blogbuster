2a50f47a05511f22684c08c5b972f0d09206f501
Bozdogan <boraozdogan99@gmail.com>
2022-03-18 15:48:45 +0300

    preliminary design

--- build.bat
+++ build.bat
@@ -1,17 +1,18 @@
 @ECHO OFF
+REM Build 3rd party distributions and my modifications to them.
 
 SETLOCAL
 SET BUILDDIR=%~dp0\blogbuster
 SET BOOTSTRAP=%~dp0\opt\bootstrap-5.1.3
 SET SASSCMD=CALL sass
-SET SASSFLAGS=--style compressed --no-source-map
+SET SASSFLAGS=--style compressed --no-source-map --load-path=%BOOTSTRAP%\scss
 
 ECHO Copying JS files...
 copy "%BOOTSTRAP%\dist\js\bootstrap.bundle.js" "%BUILDDIR%\js\" || GOTO FAIL
 
 ECHO.
 ECHO Transpiling SCSS files...
-%SASSCMD% %SASSFLAGS% "%BOOTSTRAP%\scss\bootstrap.scss" "%BUILDDIR%\css\bootstrap.min.css" || GOTO FAIL
+%SASSCMD% %SASSFLAGS% "%BUILDDIR%\scss":"%BUILDDIR%\css" || GOTO FAIL
 
 GOTO END
 
