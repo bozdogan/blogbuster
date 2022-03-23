34c77d55ae36fe0df6d5198cc20c2b68e05c78a3
Bozdogan <boraozdogan99@gmail.com>
2022-03-23 16:35:14 +0300

    enhance build script

--- build.bat
+++ build.bat
@@ -2,17 +2,29 @@
 REM Build 3rd party distributions and my modifications to them.
 
 SETLOCAL
-SET BUILDDIR=%~dp0\blogbuster
+SET SOURCEDIR=%~dp0\src
+SET ASSETSDIR=%~dp0\static
+SET BUILDDIR=%~dp0\dist
+
 SET BOOTSTRAP=%~dp0\opt\bootstrap-5.1.3
 SET SASSCMD=CALL sass
 SET SASSFLAGS=--style compressed --no-source-map --load-path=%BOOTSTRAP%\scss %1
 
+IF NOT EXIST "%BUILDDIR%" (
+    ECHO Creating build directory.
+    mkdir "%BUILDDIR%" )
+
 ECHO Copying JS files...
+IF NOT EXIST "%BUILDDIR%\js\" ( mkdir "%BUILDDIR%\js" )
 copy "%BOOTSTRAP%\dist\js\bootstrap.bundle.js" "%BUILDDIR%\js\" || GOTO FAIL
 
 ECHO.
 ECHO Transpiling SCSS files...
-%SASSCMD% %SASSFLAGS% "%BUILDDIR%\scss":"%BUILDDIR%\css" || GOTO FAIL
+IF NOT EXIST "%BUILDDIR%\css\" ( mkdir "%BUILDDIR%\css" )
+%SASSCMD% %SASSFLAGS% "%SOURCEDIR%\scss":"%BUILDDIR%\css" || GOTO FAIL
+
+ECHO Copying static assets...
+copy "%ASSETSDIR%\*" "%BUILDDIR%" || GOTO FAIL
 
 GOTO END
 
